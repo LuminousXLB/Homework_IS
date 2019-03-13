6ba593595c7442928f11040f73f20fd9dcfa82ec
Taraxacum <john980118@163.com>
2019-03-13 19:44:40 +0800

    reformat

--- lab01/clock_divisor.v
+++ lab01/clock_divisor.v
@@ -1,43 +1,43 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    23:38:03 03/03/2019 
-// Design Name: 
-// Module Name:    clock_divisor 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Company:
+// Engineer:
 //
-// Dependencies: 
+// Create Date:    23:38:03 03/03/2019
+// Design Name:
+// Module Name:    clock_divisor
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module clock_divisor(
-    output out,
-    input clk
-    );
+           output out,
+           input clk
+       );
 
 reg [31:0]counter = 32'b0;
 always @(posedge clk) begin
-	if (counter == 32'd25_000_000) begin
-		counter <= 32'b0;
-	end
-	else
-		counter <= counter + 1;
+    if (counter == 32'd25_000_000) begin
+        counter <= 32'b0;
+    end
+    else
+        counter <= counter + 1;
 end
 
 assign count_enable = (counter == 32'b0);
 
 reg out_buf = 1'b0;
 always @(posedge clk) begin
-	if(count_enable)
-		out_buf <= ~out_buf;
+    if(count_enable)
+        out_buf <= ~out_buf;
 end
 
 BUFG bufg(.O(out), .I(out_buf));
